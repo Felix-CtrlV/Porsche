20b7d6b8b53349ec786863e303382b1c127bc62f
Mr.Ant <enthtooaung1234@gmail.com>
2025-10-03 21:45:23 +0630

    manager of staff view table changing

---
+++ src/main/java/Controllers/managerInventoryController.java
@@ -0,0 +1,134 @@
+package Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class managerInventoryController {
+
+    @FXML
+    private Button cancelBtn;
+
+    @FXML
+    private Button carbtn1;
+
+    @FXML
+    private Button confirmBtn;
+
+    @FXML
+    private RadioButton electricRadio;
+
+    @FXML
+    private TextField exteriorColorField;
+
+    @FXML
+    private ToggleGroup fuelTypeGroup;
+
+    @FXML
+    private RadioButton gasolineRadio;
+
+    @FXML
+    private RadioButton hybridRadio;
+
+    @FXML
+    private TextField interiorColorField;
+
+    @FXML
+    private Button inventoryAdd;
+
+    @FXML
+    private ComboBox<?> inventoryBox;
+
+    @FXML
+    private HBox inventoryPane;
+
+    @FXML
+    private TextField modelField;
+
+    @FXML
+    private VBox modelsPane;
+
+    @FXML
+    private GridPane modelsShowBox;
+
+    @FXML
+    private Button partbtn1;
+
+    @FXML
+    private TextField priceField;
+
+    @FXML
+    private TextField productionYearField;
+
+    @FXML
+    private TextField quantityField;
+
+    @FXML
+    private TextField searchBar;
+
+    @FXML
+    private Button searchBtn;
+
+    @FXML
+    private VBox seriesPane;
+
+    @FXML
+    private CheckBox seriesSelectAll;
+
+    @FXML
+    private TextField trimField;
+
+    @FXML
+    void clickInventoryAdd(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickSearchBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickSelect718(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickSelect911(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickSelectCayenne(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickSelectMacan(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickSelectPanamera(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickSelectTaycan(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickSeriesSelectAll(ActionEvent event) {
+
+    }
+
+}
