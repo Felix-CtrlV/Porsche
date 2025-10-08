2d458d84677b394932a0426e8c4e8384b8f0e459
ASUS <maurice952003@gmail.com>
2025-10-08 11:10:11 +0630

    pp

--- src/main/java/Controllers/staffassetController.java
+++ src/main/java/Controllers/staffassetController.java
@@ -2,9 +2,13 @@ package Controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.ImageView;
+
+import java.io.IOException;
 
 public class staffassetController {
 
@@ -20,14 +24,32 @@ public class staffassetController {
     @FXML
     private ScrollPane scrollpane;
 
+    // Keep references to loaded views
+    private Parent externalView;
+    private Parent internalView;
+
     @FXML
     void clickExternalbtn(ActionEvent event) {
-
+        scrollpane.setContent(externalView); // just swap, no reload
     }
 
     @FXML
     void clickInternalbtn(ActionEvent event) {
-
+        scrollpane.setContent(internalView); // just swap, no reload
     }
 
+    @FXML
+    void initialize() {
+        try {
+            // Load both FXML files only once
+            externalView = FXMLLoader.load(getClass().getResource("/View/staffexternalcart.fxml"));
+            internalView = FXMLLoader.load(getClass().getResource("/View/staffinternalcart.fxml"));
+
+            // Set default view (e.g., internal)
+            scrollpane.setContent(internalView);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
