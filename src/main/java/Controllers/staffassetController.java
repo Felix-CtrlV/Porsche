70a4f238b2177939a0db71069cc81f87f4b1da2c
ASUS <maurice952003@gmail.com>
2025-10-08 09:49:37 +0630

    pp

---
+++ src/main/java/Controllers/staffassetController.java
@@ -0,0 +1,33 @@
+package Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.ImageView;
+
+public class staffassetController {
+
+    @FXML
+    private ImageView img;
+
+    @FXML
+    private Button internalbtn;
+
+    @FXML
+    private Button externalbtn;
+
+    @FXML
+    private ScrollPane scrollpane;
+
+    @FXML
+    void clickExternalbtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickInternalbtn(ActionEvent event) {
+
+    }
+
+}
