2b0a9ddc8e169637a4dcd7f4a1c0551fbcde5335
ASUS <maurice952003@gmail.com>
2025-10-09 10:34:14 +0630

    pp

--- src/main/java/Controllers/staffShopingCartController.java
+++ src/main/java/Controllers/staffShopingCartController.java
@@ -87,13 +87,19 @@ public class staffShopingCartController {
         carModel = "S 500 4MATIC";
         basePrice = 115000.00;
 
-        accessories.add(new Accessory("Premium Sound System", 2500.00));
         accessories.add(new Accessory("Leather Interior Upgrade", 1800.00));
         accessories.add(new Accessory("Advanced Driver Assistance", 1200.00));
         accessories.add(new Accessory("Panoramic Sunroof", 1500.00));
         accessories.add(new Accessory("Custom Alloy Wheels", 800.00));
 
         accessoriesTable.setItems(accessories);
+    }
+    public void addAccessory(String name, double price) {
+        Accessory accessory = new Accessory(name, price);
+        accessories.add(accessory);
+        accessoriesTable.setItems(accessories); // refresh table if needed
+        calculateTotals();
+        displayData();
     }
 
     /**
