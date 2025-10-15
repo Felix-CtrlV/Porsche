492c59f1acedf80e2d26e810a9742a41f8c34d3b
KPPS <khantp348@gmail.com>
2025-10-15 09:34:34 +0630

    kk

--- src/main/java/Database/DatabaseConnectionManager.java
+++ src/main/java/Database/DatabaseConnectionManager.java
@@ -45,6 +45,7 @@ public class DatabaseConnectionManager {
         logger.info("Database connection pool initialized successfully");
     }
 
+
     private Properties loadProperties() {
         Properties props = new Properties();
         try (InputStream input = getClass().getClassLoader().getResourceAsStream("application.properties")) {
