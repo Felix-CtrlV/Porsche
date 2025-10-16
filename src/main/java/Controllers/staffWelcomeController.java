0fc13d479d52bad2383ce6407a2c5cebea90dd87
lonel <lix718309@gmail.com>
2025-10-16 22:43:02 +0630

    checkout and button

--- src/main/java/Controllers/staffWelcomeController.java
+++ src/main/java/Controllers/staffWelcomeController.java
@@ -1,5 +1,6 @@
 package Controllers;
 
+import Utils.defaultStage;
 import javafx.beans.binding.DoubleBinding;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -25,23 +26,19 @@ public class staffWelcomeController {
     @FXML private Label headingLabel;
     @FXML private Button discoverbtn;
     @FXML private Button logoutbtn;
+    @FXML private Button accessorybtn;
     @FXML private ImageView backgroundImage;
     @FXML private StackPane imageContainer;
-    @FXML
-    private Button accessorybtn;
 
     public void initialize() {
-        // Load background image
         Image image = new Image(Objects.requireNonNull(
                 getClass().getResource("/Image/startUpImage.png")
         ).toExternalForm());
         backgroundImage.setImage(image);
 
-        // Bind the image size to container
         backgroundImage.fitWidthProperty().bind(imageContainer.widthProperty());
         backgroundImage.fitHeightProperty().bind(imageContainer.heightProperty());
 
-        // Dynamic font scaling
         vbox.sceneProperty().addListener((obs, oldScene, newScene) -> {
             if (newScene != null) {
                 DoubleBinding widthScale = newScene.widthProperty().divide(1188.0);
@@ -70,16 +67,20 @@ public class staffWelcomeController {
     @FXML
     private void logout(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/View/login.fxml")));
+        Scene scene = new Scene(root);
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setScene(new Scene(root));
+        defaultStage ds = new defaultStage();
+        ds.setStage(stage);
+        stage.setScene(scene);
         stage.centerOnScreen();
         stage.show();
     }
+
     @FXML
-    void clickaccessorybtn(ActionEvent event) throws IOException {
-    Parent root=FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/View/staffasset.fxml")));
-    Stage stage=(Stage) ((Node) event.getSource()).getScene().getWindow();
-    stage.setScene(new Scene(root));
-    stage.show();
+    private void clickaccessorybtn(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/View/staffasset.fxml")));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 }
